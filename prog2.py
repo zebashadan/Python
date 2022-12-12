e107be547af4ec9e6f7571bed839ac738b624633
zebashadan <119686165+zebashadan@users.noreply.github.com>
2022-12-12 18:35:36 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,9 @@
+a = [19,19,15,5,5,5,1,2]
+length = len(a)
+print(length)
+count = a.count(5)
+print("count of 5 is:",count)
+if(length==8) and (count==3):
+    print("True")
+else:
+    print("False")
