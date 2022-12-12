24e6f45ac3c3326433dd151ad21564ac9fc7c583
zebashadan <119686165+zebashadan@users.noreply.github.com>
2022-12-12 14:02:29 +0530

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
-print("hello World")
-a = 10
-b = 20
-if(a<b): 
-   print("a is less than b")
+a = [19,15,15,5,3,35,2]
+print(a)
+count = a.count(19)
+print("count of 19 is:",count)
+count = a.count(5)
+print("count of 5 is:",count)
+if(count==2) and (count==3):
+    print(True)
+else:
+    print("False")
